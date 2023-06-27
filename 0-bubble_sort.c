014cdb3edc9a8120ed552efd383691e728a0123c
maqamylee0 <maqamylee0@gmail.com>
2023-06-26 20:40:37 -0700

    sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * sort_b- iterates while swapping
+ * @array: array to swap
+ * @size: size of array
+ * @position: swaps made
+ * Return: number of swaps
+ */
+int sort_b(int *array, size_t size, size_t position)
+{
+	int temp;
+	size_t i, j, k;
+
+	j = position;
+	k = position;
+	for (i = j; i < size - 1; i++)
+        {
+		if (array[j] > array[j + 1])
+                {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+			print_array(array, size);
+			return j + 1;
+                }
+		j++;
+        }
+	if (k == 0)
+		return 0;
+	return j;
+}
+/**
+ * bubble_sort- sorts using bubble sort algortithm
+ * @array: array to sort
+ * @size: number of elements in array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t position = 0;
+
+	position = sort_b(array, size, position);
+	print_array(array, size);
+	while (position != 0)
+	{
+		if (position == size - 1)
+			position = 0;
+		position = sort_b(array, size, position);
+	}
+}
